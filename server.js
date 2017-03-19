2a19d5f3ac9897c84890810684563def3fd77596
Simen Haugerud Granlund <simen@hgranlund.com>
2017-03-19 19:23:44 +0100

    Cleaning up memory when user disconects from game

--- server.js
+++ server.js
@@ -10,11 +10,47 @@ var displayBoard = function (chess) {
   }
 };
 
+var print_result = function (chess) {
+
+  if (chess.in_checkmate()) {
+    console.log('in_checkmate')
+  }
+
+  if (chess.insufficient_material()) {
+    console.log('insufficient_material')
+  }
+
+  if (chess.in_stalemate()) {
+    console.log('in_stalemate')
+  }
+
+  if (chess.in_threefold_repetition()) {
+    console.log('in_threefold_repetition')
+  }
+
+  if (chess.in_draw()) {
+    console.log('Its a draw');
+  } else {
+    console.log((chess.turn() ? 'White' : 'Black') + ' won the game');
+  }
+};
+
 board.on('connect', function () {
   console.log('Connected to board');
 });
 
 io.on('connect', function (player) {
+
+  player.on('disconnect', function () {
+    var gamesToRemove = []
+    for (gameId in games) {
+      if (games[gameId] = player.id) {
+        gamesToRemove.push(gameId);
+      }
+    }
+    gamesToRemove.forEach(function (gameId) { delete games[gameId] })
+    console.log('Player is disconected');
+  })
 
   player.on('join', function (gameId) {
     if (games[gameId] == undefined) {
@@ -49,28 +85,3 @@ io.on('connect', function (player) {
     }
   });
 });
-
-function print_result(chess) {
-
-  if (chess.in_checkmate()) {
-    console.log('in_checkmate')
-  }
-
-  if (chess.insufficient_material()) {
-    console.log('insufficient_material')
-  }
-
-  if (chess.in_stalemate()) {
-    console.log('in_stalemate')
-  }
-
-  if (chess.in_threefold_repetition()) {
-    console.log('in_threefold_repetition')
-  }
-
-  if (chess.in_draw()) {
-    console.log('Its a draw');
-  } else {
-    console.log((chess.turn() ? 'White': 'Black') + ' won the game');
-  }
-}
