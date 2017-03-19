8c3fb3a720d80f7e75466bb5f82bd2be04052d42
Simen Haugerud Granlund <simen@hgranlund.com>
2017-03-19 02:16:40 +0100

    Added event join to the game

--- server.js
+++ server.js
@@ -1,58 +1,69 @@
 var io = require('socket.io')(3000);
 var Chess = require('chess.js').Chess;
-var player = null;
+var games = {};
 
-io.on('connect', function(socket) {
+io.on('connect', function (player) {
 
-  socket.on('move', function(game) {
-    console.log('The move: ' + game.move);
+  player.on('join', function (gameId) {
+    if (games[gameId] == undefined) {
+      console.log('Player one joined game ' + gameId + '!');
+      games[gameId] = player.id;
+    } else {
+      var chess = new Chess();
+      console.log(gameId + ' started!');
+      console.log(chess.ascii());
+      player.emit('move', {
+        board: chess.fen(),
+        white: games[gameId],
+        black: player.id
+      });
+      delete games[gameId];
+    }
+  })
 
+  player.on('move', function (game) {
     var chess = new Chess();
     chess.load(game.board);
+    console.log('Move from ' + (chess.turn() ? 'white' : 'black') + ' player: ' + game.move);
     chess.move(game.move);
     console.log(chess.ascii());
 
     if (chess.game_over()) {
-        console.log('Game over! The winning socket is. ' + socket.id )
-        console.log(chess.pgn());
-        
-        if (chess.in_checkmate()) {
-            console.log('in_checkmate')
-        }
-        
-        if (chess.insufficient_material()) {
-            console.log('insufficient_material')
-        }
-
-        if (chess.in_stalemate()) {
-            console.log('in_stalemate')
-        }
-
-        if (chess.in_threefold_repetition()) {
-            console.log('in_threefold_repetition')
-        }
-
-        if (chess.in_draw()){
-            console.log('Its a draw');
-        }
-    }
-    else{  
-        game.board = chess.fen();
-        var currentPlayer = (chess.turn() === 'w') ? game.white: game.black;
-        socket.broadcast.to(currentPlayer).emit('move', game);
+      print_result(chess);
+    } else {
+      game.board = chess.fen();
+      var nextPlayer = (chess.turn() === 'b') ? game.white : game.black;
+      console.log(nextPlayer)
+      console.log(player.id)
+      console.log(game)
+      console.log(chess.turn())
+      
+      player.broadcast.to(nextPlayer).emit('move', game);
     }
   });
-
-  if (player != null) {
-    var chess = new Chess();
-    var game = {
-      board : chess.fen(),
-      white : player,
-      black : socket.id
-    };
-    player = null;
-    io.emit('move', game);    
-  } else {
-    console.log('We got ourselves a player!');
-    player=socket.id}
 });
+
+function print_result(chess) {
+
+  if (chess.in_checkmate()) {
+    console.log('in_checkmate')
+  }
+
+  if (chess.insufficient_material()) {
+    console.log('insufficient_material')
+  }
+
+  if (chess.in_stalemate()) {
+    console.log('in_stalemate')
+  }
+
+  if (chess.in_threefold_repetition()) {
+    console.log('in_threefold_repetition')
+  }
+
+  if (chess.in_draw()) {
+    console.log('Its a draw');
+  } else {
+    console.log((chess.turn() ? 'White': 'Black') + ' won the game');
+  }
+}
