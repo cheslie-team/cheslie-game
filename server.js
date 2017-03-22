08cfbeb96ebb01ec0419b38be05f825990af5312
Simen Haugerud Granlund <simen@hgranlund.com>
2017-03-22 23:05:24 +0100

    Removed global gamesState from server

--- server.js
+++ server.js
@@ -1,53 +1,35 @@
 var config = require('./config/config'),
-  io = require('socket.io')(config.port)
+  io = require('socket.io')(config.port),
   Chess = require('chess.js').Chess,
-  feed = require('./modules/feed.js'),
-  games = {}; //games[gameId] = players-one-socket-id
+  feed = require('./modules/feed.js');
 
 feed.init(io);
-
 io.on('connect', function (socket) {
-  socket.on('disconnect', function () {
-    var gamesToRemove = []
-    for (gameId in games) {
-      if (games[gameId] = socket.id) {
-        gamesToRemove.push(gameId);
-      }
-    }
-    gamesToRemove.forEach(function (gameId) { delete games[gameId] })
-  })
-
   socket.on('join', function (gameId) {
-    if (games[gameId] == undefined) {
-      games[gameId] = socket.id;
-    } else {
+    socket.join(gameId);
+    var allPlayers = io.sockets.adapter.rooms[gameId];
+    if (allPlayers.length === 2) {
       var chess = new Chess();
       feed.gameStarted(gameId);
       socket.emit('move', {
         id: gameId,
         board: chess.fen(),
-        white: games[gameId],
-        black: socket.id
       });
-      delete games[gameId];
     }
   })
 
   socket.on('move', function (game) {
-    var chess = new Chess();
-    chess.load(game.board);
+    var chess = new Chess(game.board);
     chess.move(game.move);
     feed.move({
       gameId: game.id,
       board: chess.fen()
     });
-
     if (chess.game_over()) {
       feed.gameEnded(game.id, chess)
     } else {
       game.board = chess.fen();
-      var nextPlayer = (chess.turn() === 'b') ? game.white : game.black;
-      socket.broadcast.to(nextPlayer).emit('move', game);
+      socket.broadcast.to(game.id).emit('move', game);
     }
   });
 });
