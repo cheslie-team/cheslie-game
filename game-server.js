08578244999d23e8e7bdbe6f9d1e6c87852031fc
Simen Haugerud Granlund <simen@hgranlund.com>
2017-09-09 23:19:24 +0200

    Fikser hvem som blir svart og hvit

--- game-server.js
+++ game-server.js
@@ -1,18 +1,30 @@
 var config = require('cheslie-config'),
   server = require('http').createServer(),
+  hash = require('hash.js'),
   io = require('socket.io').listen(server),
   Game = require('./modules/game.js').Game,
   feed = require('./modules/feed.js').init(io),
   playerNames = {};
 
+var isWhiteBlack = function (whiteName, blackName, gameId) {
+  var key = whiteName + blackName;
+  var hashedKey = hash.sha256().update(key).digest('hex');
+  return gameId.startsWith(hashedKey);
+}
+
 io.on('connect', function (socket) {
   socket.on('join', function (gameId, playerName) {
     playerNames[socket.id] = playerName;
     socket.join(gameId);
-
     var allPlayers = Object.keys(io.sockets.adapter.rooms[gameId].sockets);
     if (allPlayers.length === 2) {
-      var game = new Game(gameId, allPlayers[1], allPlayers[0], playerNames);
+      var game
+      if (isWhiteBlack(playerNames[allPlayers[1]], playerNames[allPlayers[0]], gameId)) {
+        game = new Game(gameId, allPlayers[1], allPlayers[0], playerNames);
+      } else {
+        game = new Game(gameId, allPlayers[0], allPlayers[1], playerNames);
+
+      }
       feed.gameStarted(game);
       socket.emit('move', game.asPublicGame());
     }
